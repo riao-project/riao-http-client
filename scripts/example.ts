144d9ea415afa4ed7495d4d8a4026a216eb2fad4
Drew Immerman <drewimmerman@gmail.com>
2024-05-04 15:59:46 -0400

    Create http client

--- scripts/example.ts
+++ scripts/example.ts
@@ -1,7 +1,74 @@
+import { RiaoHttpClient } from '../src';
+
+export class User {
+	id?: number;
+	username: string;
+	password: string;
+}
+
+const user: User = {
+	username: 'apitest',
+	password: 'password123',
+};
+
+class UserHttpClient extends RiaoHttpClient<User> {
+	url = 'http://localhost:3000/api/v1/users';
+}
+
+const userRepo = new UserHttpClient();
+
 /**
  * Do something!
  */
 export default async function example(): Promise<void> {
 	/* eslint-disable-next-line no-console */
 	console.log('Hello!');
+
+	// Create a single user
+	const newUser = await userRepo.postOne(user);
+	console.log('Created user', newUser);
+
+	// Get all users
+	const users = await userRepo.getMany({ limit: 3 });
+	console.log('Got all users', users);
+
+	// Get one user
+	if (newUser.id) {
+		const getNewUser = await userRepo.getOne({ id: newUser.id });
+		console.log('Get new user', getNewUser);
+	}
+	else {
+		console.log('No new user to get');
+	}
+
+	// Login
+	if (newUser.id) {
+		await userRepo.action('login', <User>{
+			username: 'bob',
+			password: 'password123',
+		});
+	}
+
+	// Patch one user
+	if (newUser.id) {
+		const patchedUser = await userRepo.patchOne({
+			id: newUser.id,
+			data: { username: 'patched' },
+		});
+		console.log('Patched new user', patchedUser);
+	}
+	else {
+		console.log('No new user to patch');
+	}
+
+	// Delete one user
+	if (newUser.id) {
+		await userRepo.deleteOne({
+			id: newUser.id,
+		});
+		console.log('Deleted the user!');
+	}
+	else {
+		console.log('No new user to patch');
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,110 @@
-export const a = true;
+import {
+	ApiRequest,
+	DeleteOneRequest,
+	DeleteOneResponse,
+	GetManyRequest,
+	GetManyResponse,
+	GetOneRequest,
+	GetOneResponse,
+	PatchOneRequest,
+	PatchOneResponse,
+	PostOneRequest,
+	PostOneResponse,
+} from '@riao/server-contract';
+
+export type DatabaseRecord = Record<string, any>;
+
+interface RiaoHttpRequest {
+	url: string;
+	method: 'GET' | 'POST' | 'PATCH' | 'DELETE';
+	body?: any;
+	queryParams?: Record<string, any>;
+}
+
+export class RiaoHttpClient<T extends DatabaseRecord = DatabaseRecord> {
+	public url = '';
+
+	public async request(options: RiaoHttpRequest): Promise<unknown> {
+		const fetchOptions: RequestInit = {
+			method: options.method,
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+		};
+
+		if (options.body) {
+			fetchOptions.body = JSON.stringify(options.body);
+		}
+
+		if (options.queryParams && Object.keys(options.queryParams).length) {
+			options.url += this.queryParams(options.queryParams);
+		}
+
+		const response = await fetch(options.url, fetchOptions);
+
+		if (options.method === 'DELETE') {
+			return response.body;
+		}
+
+		return response.json();
+	}
+
+	protected queryParams(params: Record<string, any>) {
+		return '?' + new URLSearchParams(params);
+	}
+
+	public async getMany(
+		request: GetManyRequest<T>
+	): Promise<GetManyResponse<T>> {
+		return <T[]>await this.request({
+			method: 'GET',
+			url: this.url,
+			queryParams: request,
+		});
+	}
+
+	public async getOne(request: GetOneRequest): Promise<GetOneResponse> {
+		return <T>await this.request({
+			method: 'GET',
+			url: `${this.url}/${request.id}`,
+		});
+	}
+
+	public async postOne(
+		request: PostOneRequest<T>
+	): Promise<PostOneResponse<T>> {
+		return <T>await this.request({
+			method: 'POST',
+			url: this.url,
+			body: request,
+		});
+	}
+
+	public async patchOne(
+		request: PatchOneRequest<T>
+	): Promise<PatchOneResponse<T>> {
+		return <T>await this.request({
+			method: 'PATCH',
+			url: `${this.url}/${request.id}`,
+			body: request.data,
+		});
+	}
+
+	public async deleteOne(
+		request: DeleteOneRequest
+	): Promise<DeleteOneResponse> {
+		await this.request({
+			method: 'DELETE',
+			url: `${this.url}/${request.id}`,
+		});
+	}
+
+	public async action(action: string, request: any | T) {
+		return await this.request({
+			method: 'POST',
+			url: `${this.url}/${action}`,
+			body: request,
+		});
+	}
+}
